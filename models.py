39c6bc564f1b6422cb0b6dfca2db0b971f30a0f8
Rielk <william@symbotics.co.uk>
2019-11-16 16:30:03 +0000

    Adam constructor now works

--- models.py
+++ models.py
@@ -5,42 +5,56 @@ Created on Sat Nov 16 11:31:39 2019
 
 @author: william
 """
-from keras.layers import Input, Dense, Dropout, BatchNormalization, Merge
+from keras.layers import Input, Dense, Dropout, BatchNormalization, concatenate
 from keras.models import Model
 from keras.constraints import maxnorm
 from generators import data_gen, default_params
 import numpy as np
 
 def Adam(params):
-    inputs_list = []
+    inputs = []
     for n in range(params["input_points"][1]):
-        inputs = Input(shape=(params["input_points"][0],), name="input{}".format(n))
-        inputs_list.append(inputs)
+        i = Input(shape=(params["input_points"][0],), name="input_{}".format(n+1))
+        inputs.append(i)
         
     
     drop_rate = params["dropout_rate"]
     max_norm = params["max_norm"]
     activation = params["layer_activation"]
     
-    for x in inputs_list:
-        try:
-            params["node_per_layer"]
-            for i,node_count in enumerate(params["node_per_layer"]):
-                x = Dense(32, activation=activation, name="dense{}".format(i+1), kernel_constraint=maxnorm(max_norm))(x)
-                x = Dropout(drop_rate)(x)
-                x = BatchNormalization()(x)
-        except KeyError:
-            params["number_of_layers"]
-            for i,node_count in enumerate(params["number_of_layer"]):
-                x = Dense(params["input_points"], activation=activation, name="dense{}".format(i+1), kernel_constraint=maxnorm(max_norm))(x)
-                x = Dropout(drop_rate)(x)
-                x = BatchNormalization()(x)
+    dense_layers = []
+    try:
+        params["node_per_layer"]
+        for i,node_count in enumerate(params["node_per_layer"]):
+            if node_count == 0:
+                merge_layer = 3*i
+                continue
+            dense_layers.append(Dense(node_count, activation=activation, name="dense_{}".format(i+1), kernel_constraint=maxnorm(max_norm)))
+            dense_layers.append(Dropout(drop_rate))
+            dense_layers.append(BatchNormalization())
+    except KeyError:
+        params["number_of_single_layers"]
+        for i,node_count in enumerate(params["number_of_single_layers"]+params["number_of collection_layers"]):
+            dense_layers.append(Dense(params["input_points"], activation=activation, name="dense_{}".format(i+1), kernel_constraint=maxnorm(max_norm)))
+            dense_layers.append(Dropout(drop_rate))
+            dense_layers.append(BatchNormalization())
+        params["number_of_single_layers"] = 3*i
+    
+    outs = []    
+    for x in inputs:
+        for layer in dense_layers[:merge_layer]:
+            x = layer(x)
+        outs.append(x)
             
     try:
         activation = params["output_activation"]
     except KeyError:
         pass
-    x = Merge(inputs_list)
+    x = concatenate(outs)
+    
+    for layer in dense_layers[merge_layer:]:
+        x = layer(x)
+    
     outputs = Dense(2, activation=activation, name="output")(x)
     model = Model(inputs=inputs, outputs=outputs)
     
@@ -53,7 +67,7 @@ def Adam(params):
 
 if __name__ == '__main__':
     model_params = {"input_points":(32,2),
-              "node_per_layer":[32,32,32,16,16],
+              "node_per_layer":[32,32,32,0,16,16],
               "dropout_rate":.2,
               "max_norm":3.,
               "layer_activation":"relu",
@@ -68,7 +82,7 @@ if __name__ == '__main__':
     data_params = default_params(600)
     data_params["angles"] = (0, np.pi/4)
     _, data, labels = data_gen(data_params)
-    data = np.array([data[:,0], data[:,1]])
+    data = [data[:,0], data[:,1]]
     #Clear the shapes from memory
     _ = None
     
@@ -79,7 +93,7 @@ if __name__ == '__main__':
     v_params["noise"] = .0011
     
     _, v_data, v_labels = data_gen(v_params)
-    v_data = np.array([v_data[:,0], v_data[:,1]])
+    v_data = [v_data[:,0], v_data[:,1]]
     #Clear the shapes from memory
     _ = None
     
