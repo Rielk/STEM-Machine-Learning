c10005e1a5c1ee97beea064cda98d255a4eb440d
Redflameman0 <william@symbotics.co.uk>
2020-03-06 11:03:36 +0000

    Added Dave

--- models.py
+++ models.py
@@ -6,10 +6,12 @@ Created on Sat Nov 16 11:31:39 2019
 @author: william
 """
 from keras.layers import Input, Dense, Dropout, BatchNormalization
-from keras.layers import concatenate, Conv1D, Reshape, Flatten
-from keras.models import Model
+from keras.layers import concatenate, Conv1D, Reshape, Flatten, MaxPool1D
+from keras.models import Model 
 from keras.constraints import maxnorm
 from generators import data_gen, data_default_params
+import keras.losses as KL
+import keras.backend as KB
 import numpy as np
 
 def Adam(params=None):
@@ -220,6 +222,88 @@ def Charlie(params=None):
                       loss={"output_main":loss,
                             "output_assister":a_loss},
                       loss_weights=loss_weights, metrics=["accuracy"])
+        return model
+    
+def Dave(params=None):
+    if params is None:
+        return "Dave"
+    else:
+        inputs = []
+        for n in range(params["input_points"][1]):
+            i = Input(shape=(params["input_points"][0],), name="input_{}".format(n+1))
+            inputs.append(i)
+        
+        drop_rate = params["dropout_rate"]
+        max_norm = params["max_norm"]
+        activation = params["layer_activation"]
+        
+        layers = []
+        mod = 1
+        layer_tracker = 0
+        merge_layer = 0
+        for i,node_count in enumerate(params["node_per_layer"]):
+            if node_count == 0:
+                if merge_layer == 0:
+                    merge_layer = layer_tracker
+                    mod = 0
+                    continue
+                else:
+                    layers.append(Flatten())
+                    layer_tracker += 1
+                    continue
+            if type(node_count) is tuple:
+                layers.append(Conv1D(node_count[0], node_count[1], activation=activation, name="conv1D_{}".format(i+mod), kernel_constraint=maxnorm(max_norm), padding="same"))
+                layers.append(MaxPool1D(node_count[2], name="max_pooling1D_{}".format(i+mod)))
+                layer_tracker += 2
+            else:
+                layers.append(Dense(node_count, activation=activation, name="dense_{}".format(i+mod), kernel_constraint=maxnorm(max_norm)))
+                layer_tracker += 1
+            layers.append(Dropout(drop_rate))
+            layers.append(BatchNormalization())
+            layer_tracker += 2
+        
+        r = Reshape((params["input_points"][0], 1))
+        outs = []    
+        for x in inputs:
+            x = r(x)
+            for layer in layers[:merge_layer]:
+                x = layer(x)
+            outs.append(x)
+                
+        try:
+            activation = params["output_activation"]
+        except KeyError:
+            pass
+        if len(outs) > 1:
+            x = concatenate(outs, axis=-1)
+        else:
+            x = outs[0]
+        
+        for layer in layers[merge_layer:]:
+            x = layer(x)
+        
+        output1 = Dense(params["output_categories"][0], activation=activation, name="output_main")(x)
+        output2 = Dense(params["output_categories"][1], activation=activation, name="output_assister")(x)
+        output = concatenate([output1, output2])
+        outputs = [output]
+        model = Model(inputs=inputs, outputs=outputs)
+        
+        optimizer = params["optimizer"]
+        loss = KL.get(params["loss"])
+        a_loss = KL.get(params["a_loss"])
+        loss_weights = params["weight_ratio"]
+        weight_main = loss_weights[0]
+        weight_assi = loss_weights[1]
+        def losses(y_true, y_pred):
+            true_main = y_true[:,:params["output_categories"][0]]
+            true_assi = y_true[:,params["output_categories"][0]:]
+            pred_main = y_pred[:,:params["output_categories"][0]]
+            pred_assi = y_pred[:,params["output_categories"][0]:]
+            loss_main = loss(true_main, pred_main)
+            loss_assi = a_loss(true_assi, pred_assi)
+            return weight_main*loss_main + weight_assi*loss_assi
+#            return KL.mean_absolute_error(y_pred, y_pred)
+        model.compile(optimizer=optimizer, loss=losses, metrics=["accuracy"])
         return model
  
 def Adam_default_params():
