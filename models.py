57e37fc85d4ab936c4ca22bcbe116873b4b0d25e
Redflameman0 <william@symbotics.co.uk>
2020-03-13 11:25:24 +0000

    Added a script for transfer learning and Eliot model for use in script

--- models.py
+++ models.py
@@ -11,7 +11,6 @@ from keras.models import Model
 from keras.constraints import maxnorm
 from generators import data_gen, data_default_params
 import keras.losses as KL
-import keras.backend as KB
 import numpy as np
 
 def Adam(params=None):
@@ -175,17 +174,26 @@ def Charlie(params=None):
         layers = []
         mod = 1
         merge_layer = 0
+        layer_tracker = 0
         for i,node_count in enumerate(params["node_per_layer"]):
             if node_count == 0:
-                merge_layer = 3*i
-                mod = 0
-                continue
+                if merge_layer == 0:
+                    merge_layer = layer_tracker
+                    mod = 0
+                    continue
+                else:
+                    layers.append(Flatten())
+                    layer_tracker += 1
+                    continue
             if type(node_count) is tuple:
                 layers.append(Conv1D(node_count[0], node_count[1], activation=activation, name="conv1D_{}".format(i+mod), kernel_constraint=maxnorm(max_norm)))
+                layer_tracker += 1
             else:
                 layers.append(Dense(node_count, activation=activation, name="dense_{}".format(i+mod), kernel_constraint=maxnorm(max_norm)))
+                layer_tracker += 1
             layers.append(Dropout(drop_rate))
             layers.append(BatchNormalization())
+            layer_tracker += 2
         
         r = Reshape((params["input_points"][0], 1))
         outs = []    
@@ -207,8 +215,6 @@ def Charlie(params=None):
         for layer in layers[merge_layer:]:
             x = layer(x)
         
-        x = Flatten()(x)
-        
         output1 = Dense(params["output_categories"][0], activation=activation, name="output_main")(x)
         output2 = Dense(params["output_categories"][1], activation=activation, name="output_assister")(x)
         outputs = [output1, output2]
@@ -223,6 +229,17 @@ def Charlie(params=None):
                             "output_assister":a_loss},
                       loss_weights=loss_weights, metrics=["accuracy"])
         return model
+    
+def Charlie_recompile(model, params):
+    optimizer = params["optimizer"]
+    loss = params["loss"]
+    a_loss = params["a_loss"]
+    loss_weights = params["weight_ratio"]
+    model.compile(optimizer=optimizer,
+                  loss={"output_main":loss,
+                        "output_assister":a_loss},
+                  loss_weights=loss_weights, metrics=["accuracy"])
+    return model    
     
 def Dave(params=None):
     if params is None:
@@ -305,7 +322,93 @@ def Dave(params=None):
 #            return KL.mean_absolute_error(y_pred, y_pred)
         model.compile(optimizer=optimizer, loss=losses, metrics=["accuracy"])
         return model
- 
+    
+def Dave_recompile(model, params):
+    optimizer = params["optimizer"]
+    loss = KL.get(params["loss"])
+    a_loss = KL.get(params["a_loss"])
+    loss_weights = params["weight_ratio"]
+    weight_main = loss_weights[0]
+    weight_assi = loss_weights[1]
+    def losses(y_true, y_pred):
+        true_main = y_true[:,:params["output_categories"][0]]
+        true_assi = y_true[:,params["output_categories"][0]:]
+        pred_main = y_pred[:,:params["output_categories"][0]]
+        pred_assi = y_pred[:,params["output_categories"][0]:]
+        loss_main = loss(true_main, pred_main)
+        loss_assi = a_loss(true_assi, pred_assi)
+        return weight_main*loss_main + weight_assi*loss_assi
+#            return KL.mean_absolute_error(y_pred, y_pred)
+    model.compile(optimizer=optimizer, loss=losses, metrics=["accuracy"])
+    return model
+    
+def Eliot(params=None):
+    if params is None:
+        return "Eliot"
+    else:
+        inputs = []
+        for n in range(params["input_points"][1]):
+            i = Input(shape=(params["input_points"][0],), name="input_{}".format(n+1))
+            inputs.append(i)
+        
+        drop_rate = params["dropout_rate"]
+        max_norm = params["max_norm"]
+        activation = params["layer_activation"]
+        
+        layers = []
+        mod = 1
+        merge_layer = 0
+        layer_tracker = 0
+        for i,node_count in enumerate(params["node_per_layer"]):
+            if node_count == 0:
+                if merge_layer == 0:
+                    merge_layer = layer_tracker
+                    mod = 0
+                    continue
+                else:
+                    layers.append(Flatten())
+                    layer_tracker += 1
+                    continue
+            if type(node_count) is tuple:
+                layers.append(Conv1D(node_count[0], node_count[1], activation=activation, name="conv1D_{}".format(i+mod), kernel_constraint=maxnorm(max_norm)))
+                layers.append(MaxPool1D(node_count[2], name="max_pooling1D_{}".format(i+mod)))
+                layer_tracker += 2
+            else:
+                layers.append(Dense(node_count, activation=activation, name="dense_{}".format(i+mod), kernel_constraint=maxnorm(max_norm)))
+                layer_tracker += 1
+            layers.append(Dropout(drop_rate))
+            layers.append(BatchNormalization())
+            layer_tracker += 2
+        
+        r = Reshape((params["input_points"][0], 1))
+        outs = []    
+        for x in inputs:
+            x = r(x)
+            for layer in layers[:merge_layer]:
+                x = layer(x)
+            outs.append(x)
+                
+        try:
+            activation = params["output_activation"]
+        except KeyError:
+            pass
+        if len(outs) > 1:
+            x = concatenate(outs, axis=-1)
+        else:
+            x = outs[0]
+        
+        for layer in layers[merge_layer:]:
+            x = layer(x)
+        
+        outputs = Dense(params["output_categories"], activation=activation, name="output_main")(x)
+        model = Model(inputs=inputs, outputs=outputs)
+        
+        optimizer = params["optimizer"]
+        loss = params["loss"]
+        
+        model.compile(optimizer=optimizer, loss=loss, metrics=["accuracy"])
+        return model
+    
 def Adam_default_params():
     return {"model":Adam,
             "input_points":(32,2),
---
+++ recognise_holes_transfer.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 16 00:32:55 2020
+
+@author: William
+"""
+from models import Eliot
+from generators import data_gen
+import matplotlib.pyplot as plt
+from runner import run
+import numpy as np
+plt.close("all")
+
+#Produce learning data
+data_params = {"data_count_c":30000,
+               "data_count_a":30000,
+               
+               "r":1.,    
+               "sig_r":.1,
+               "angle_count":90,
+               "centre":(0,0),
+               "sig_c":(.0,.0),#(.3,.3),
+               "sig":.05,
+               "h_range":.4,
+               "h_r":.075,
+               "sig_hr":.0075,
+               "s_range":.05,
+               "s_r":.6,
+               "sig_sr":.02,
+               "s_density":1.,
+               "sig_sd":.0,
+               "s_s":.04,
+               "sig_ss":.008,
+               "s_t":(.01,.1),
+               "sig_st":(.002,.02),
+               
+               "data_points":64,
+               "angles":(0., np.pi/2),
+               "sig_a":.01,
+               "about":(0.,0.),
+               "lower":-1.5,
+               "upper":1.5,
+               "background":20.,
+               "gauss":.5,
+               "noise":.001,
+               "limit_var":.05
+               }
+
+v_params = data_params.copy()
+v_params["data_count_c"] = 10000
+v_params["data_count_a"] = 10000
+
+train_params = {"model":Eliot,
+                "epochs":200,
+                "verification":True,
+                "patience":40,
+                "restore_best_weights":True
+                }
+
+#Make the model
+net_structure = [64 for _ in range(4)]
+net_structure += [(64,4,1) for _ in range(4)]
+net_structure += [0]
+net_structure += [64 for _ in range(2)]
+net_structure += [32 for _ in range(4)]
+net_structure += [16 for _ in range(4)]
+net_structure += [0]
+model_params = {"model":Eliot,
+            "input_points":(64,2),
+            "output_categories":2,
+            "node_per_layer":net_structure,
+            "dropout_rate":.1,
+            "max_norm":3.,
+            "layer_activation":"relu",
+            "output_activation":"softmax",
+            "optimizer":"Adagrad",
+            "loss":"categorical_crossentropy",
+            }
+
+for n1 in range(3):
+    print("Training on spirals, no holes")
+    model, accuracy1, epoch1, t1 = run(train_params, model_params, data_params, v_params, True)
+    if accuracy1 >= .75:
+        break
+else:
+    raise KeyboardInterrupt("Too many attempts")
+
+for n2 in range(3):
+    data_params["data_count_h"] = 15000
+    data_params["data_count_s"] = 15000
+    del data_params["data_count_c"]
+    del data_params["data_count_a"]
+    
+    v_params["data_count_h"] = 5000
+    v_params["data_count_s"] = 5000
+    del v_params["data_count_c"]
+    del v_params["data_count_a"]
+    
+    print("Training on spirals, with holes")
+    model, accuracy2, epoch2, t2 = run(train_params, model_params, data_params, v_params, True, model=model)
+    if accuracy2 >= .75:
+        break
+else:
+    raise KeyboardInterrupt("Too many attempts")
+
+t = t1 + t2
+
+v_params["data_count_h"] = 100
+v_params["data_count_s"] = 100
+shapes, data, labels = data_gen(v_params)
